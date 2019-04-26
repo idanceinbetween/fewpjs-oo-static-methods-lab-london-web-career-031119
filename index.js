a8d7f9d547a13026ee4d399c7031f536083c5b2c
github username <github email address>
2019-04-26 08:52:17 +0100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let arr = string.split(" ")
+    let newArr = new Array()
+    newArr.push(this.capitalize(arr.shift()))
+
+    let exclusion = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    arr.map(word => (exclusion.find(exc => exc == word)) ? newArr.push(word) : newArr.push(this.capitalize(word)))
+    return newArr.join(" ")
+  }
+  //in the range except the first element, if word is part of []
+
 }
